809ecd9d6b8f458958e5ba45513d69ff406fa35f
Stephen Yang <stepheny755@gmail.com>
2019-01-23 03:00:00 +0000

    added bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+import asyncio
+
+TOKEN = 'NTM3NDQ4MzA0MDM2ODA2NzAx.DylZ2w.czaCIm0N0qoA9eNgI2EC8jLNGV4'
+
+client = discord.Client()
+
+@client.event
+async def on_read():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(TOKEN)
