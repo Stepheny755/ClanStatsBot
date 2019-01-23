4112630a02734caadcd1c589dcd7100d4a5f84b1
Stephen Yang <stepheny755@gmail.com>
2019-01-23 04:54:37 +0000

    updated bot.py

--- clan.py
+++ clan.py
@@ -1,24 +1,29 @@
 import time
 
+monthtime = 2592000
+renametime = 1547524924
+
+
 class Clan():
 
     def __init__(self):
         pass        
 
+
+
+
+
 class Time():
 
-    monthtime = 2592000
-    renametime = 1547524924
-
+    
     def __init__(self):
         pass
 
-    def available(self,timezone):
+    def available(timezone):
         if(timezone=='EST'):
             temp = time.ctime(monthtime+renametime-5*3600)
         if(timezone=='PST'):
             temp = time.ctime(monthtime+renametime-8*3600)
-
-    def available(self):
-        temp = time.ctime(monthtime+renametime)
+        if(timezone=='UTC'):
+            temp = time.ctime(monthtime+renametime)
         return temp
