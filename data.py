2752874a89ac9d3c419eb1a632f139a1d4d7f92d
Stepheny755 <stepheny755@gmail.com>
2019-01-30 20:36:55 -0800

    updated data.py

--- data.py
+++ data.py
@@ -1,8 +1,28 @@
 import numpy as np
 import json
+import csv
+import os
+
+#dir = "Data"
 
 class Data():
     def __init__(self):
         pass
 
 
+    def read(self,filename):
+        with open(filename,'r') as f: #os.path.join(dir,filename).strip(),'r') as f:
+            print(f)
+            input = csv.reader(f,delimiter=',')
+            print(input)
+            for row in input:
+                print(row)
+                print(', '.join(row))
+                print(input)
+
+
+
+if(__name__=="__main__"):
+    d = Data()
+    d.read('Book1.csv')
+    print("done")
