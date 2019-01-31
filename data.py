b897f6ef30eb1e383d7504ced01425a7c8047e04
Stephen Yang <stepheny755@gmail.com>
2019-01-31 12:20:57 -0800

    Update data.py

--- data.py
+++ data.py
@@ -10,6 +10,7 @@ class Data():
     def __init__(self):
         pass
 
+    
 
     def testread(self,filename):
         rdarr = []
@@ -22,10 +23,16 @@ class Data():
             out = csv.writer(w)
             out.writerows(output)
     
-
-
-
-
+    def read(self,filename):
+        rdarr = []
+        with open(os.path.join(adir,filename).strip(),'r') as r:
+            rdarr = list(csv.reader(r,delimiter=','))
+        return rdarr
+ 
+    def write(self,filename,output):
+        with open(os.path.join(adir,filename).strip(),'w+') as w:
+            out = csv.writer(w)
+            out.writerows(output)
 
 
 if(__name__=="__main__"):
