316ca0b62eaf864c5c0048e6a913e20340a87cb3
Stephen Yang <stepheny755@gmail.com>
2019-02-04 14:23:22 -0800

    Update stats.py

--- stats.py
+++ stats.py
@@ -30,34 +30,6 @@ class Stats():
 
         return (700*nDmg + 300*nKills + 150*nWR)
 
-    def saveExpValues(self):
-        dt = Data('test')
-        api = API()
-
-        temp = api.expectedValues()
-        time = temp['time']
-
-        out = []
-        i = 0
-        for shipid,shipdata in temp['data'].items():
-            lst = []
-            lst.append(shipid)
-            print(shipid)
-            name = api.getShipName(shipid)
-            if name is not None:
-                lst.append(name)
-            #print(api.getShipName(shipid))
-            for key in shipdata:
-                lst.append(shipdata[key])
-                #print(shipdata[key])
-            out.append(lst)
-
-        #print(out)
-
-        dt.write('wowsnumbers',(str(time)+'.csv').strip(),out)
-
-        return temp['data']
-
 if(__name__=="__main__"):
     #a = float(input("dmg: "))
     #b = float(input("kills: "))
