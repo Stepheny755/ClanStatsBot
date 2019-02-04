466a67be5496c99910245ad1ccb6dd1324dd267e
Stephen Yang <stepheny755@gmail.com>
2019-02-04 14:24:33 -0800

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,33 @@
 
+class Update():
+    def saveExpValues(self):
+        dt = Data('test')
+        api = API()
+
+        temp = api.expectedValues()
+        time = temp['time']
+
+        out = []
+        i = 0
+        for shipid,shipdata in temp['data'].items():
+            lst = []
+            lst.append(shipid)
+            print(shipid)
+            name = api.getShipName(shipid)
+            if name is not None:
+                lst.append(name)
+            #print(api.getShipName(shipid))
+            for key in shipdata:
+                lst.append(shipdata[key])
+                #print(shipdata[key])
+            out.append(lst)
+
+        #print(out)
+
+        dt.write('wowsnumbers',(str(time)+'.csv').strip(),out)
+
+        return temp['data']
+
+
+if(__name__=="__main__"):
+ 
