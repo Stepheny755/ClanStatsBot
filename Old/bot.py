f902f7be5b6d49d0fdc268df3a1b77c6d38b068c
Stepheny755 <stepheny755@gmail.com>
2019-02-04 14:35:32 -0800

    updated update.py

--- Old/bot.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import json
-
-import aiohttp
-
-TOKEN=NTM3NDQ4MzA0MDM2ODA2NzAx.DylZ2w.czaCIm0N0qoA9eNgI2EC8jLNGV4
-URL= "https://discordapp.com/api"
-
-async def api_call(path):
--- update.py
+++ update.py
@@ -10,7 +10,6 @@ class Update():
         time = temp['time']
 
         out = []
-        i = 0
         for shipid,shipdata in temp['data'].items():
             lst = []
             lst.append(shipid)
@@ -18,18 +17,13 @@ class Update():
             name = api.getShipName(shipid)
             if name is not None:
                 lst.append(name)
-            #print(api.getShipName(shipid))
             for key in shipdata:
                 lst.append(shipdata[key])
-                #print(shipdata[key])
             out.append(lst)
 
         #print(out)
-
         dt.write('wowsnumbers',(str(time)+'.csv').strip(),out)
-
         return temp['data']
-
 
 if(__name__=="__main__"):
     u = Update()
