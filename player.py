5264153f052546b240e798a78140b7f21f4ca030
Stephen Yang <stepheny755@gmail.com>
2019-02-12 23:07:44 -0800

    updated player.py

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 from API import API
 from data import Data
+import json
 
 class Player():
 
-    playerName = ''
-    playerID = ''
+    lbt = 0 #last_battle_time
+    playerID = 0 #account_id
+    playerName = '' #nickname
 
     avgDmg = 0
     avgKills = 0
@@ -17,6 +19,8 @@ class Player():
     draws = 0
     battles = 0
 
+    distance = 0
+
     def __init__(self,clan,name):
         #TODO:get Player 'name' Data from WG API
         api = API()
@@ -27,7 +31,31 @@ class Player():
 
         #TODO:If directory playerclan/playername does not exist and contains no files, then pull data directly from wg api without saving (temp lookup)
         temppath = str(str(clan).strip()+"/"+str(name).strip())
-        print(dt.getSMostRecent(temppath)['last_battle_time'])
+        filestring = dt.getSMostRecent(temppath)
+        #print(filestring)
+        filestring = filestring.replace("'",'"')
+        filestring = filestring.replace("None",'0')
+        filestring = filestring.replace("False",'0')
+        #print(filestring)
+        #print(filestring[189])
+        lst = json.loads(filestring)
+        print(lst)
+        print()
+
+        stats = lst['statistics']
+        self.battles = stats['battles']
+        self.distance = stats['distance']
+
+        stats = stats['pvp']
+
+        for i in stats:
+            print(i)
+
+
+        for i in lst:
+            #print(str(i)+": "+str(lst[i]))
+            break
+        #print(lst['statistics'])
 
         #print(Player.calcwr(self.wins,self.losses))
         pass
