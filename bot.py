49e7137149ca7478dd068a57641c3b92cee2b825
Stephen Yang <stepheny755@gmail.com>
2019-02-16 18:53:07 -0800

    updated bot.py

--- bot.py
+++ bot.py
@@ -86,6 +86,12 @@ async def on_message(message):
         ret = str(api.getPlayerStats(playerID))[:2000]
         await client.send_message(message.channel,ret)
 
+    elif(message.content.startswith('!embed')):
+        embed = discord.Embed(title="Tile", description="Desc", color=0x00ff00)
+        embed.add_field(name="Field1", value="hi", inline=False)
+        embed.add_field(name="Field2", value="hi2", inline=False)
+        await client.send_message(message.channel, embed=embed)
+
     print(str(message.channel)+": "+message.content)
 
 def scheduled_job():
