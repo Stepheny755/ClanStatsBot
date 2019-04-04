e36e792126b7745555eacc538befddb0c0d87a5b
jcw780 <30562395+jcw780@users.noreply.github.com>
2019-04-03 23:51:07 -0400

    Add files via upload

--- GPSC.py
+++ GPSC.py
@@ -1,14 +1,17 @@
 from API import API
+from util import Util
 import pickle
 import numpy as np
 from stats_v import Stats
 import operator
+import time 
 
 class ClanStats:
     def __init__(self, CL):
         self.clanList = CL #List of Clans
         self.a = API()
         self.s = Stats()
+        self.u = Util()
     
     def getAllPlayers(self): #Gets every in clan within clanlist
         print('Starting Player Collection')
@@ -34,23 +37,12 @@ class ClanStats:
     def updateExpected(self):
         print('Acquiring Expected')
         r = self.a.expectedValues()
-        self.time = r['time']
+        self.eTime = r['time']
+        self.sTime = self.u.getGMTTime()
         self.expected = r['data']
 
         self.shipLength = len(self.expected)
         print('Acquisition Completed')
-
-    def saveExpected(self): #Kind of pointless atm
-        print('Saving...')
-        with open('Expected-' + str(self.time) + '.pkl', 'wb') as f:
-            pickle.dump([self.time, self.expected, self.SID],f)
-        print('Save Completed')
-    
-    def getExpected(self, name): #Kind of pointless atm
-        with open(name, 'rb') as f:
-            self.time, self.expected, self.SID = pickle.load(f)
-            
-        self.shipLength = len(self.expected)
 
     def getShipIDs(self): #Gets every ship ID within expected - No longer needed
         self.SID = {} #{name: SID...}
@@ -88,6 +80,12 @@ class ClanStats:
                 
     #^ WOWSNUMBERS STUFF ^
     def getTotalPlayerStats(self):
+        print('Acquiring Player Ship Stats')
+        self.playerShipStats = np.zeros((self.totalPlayers, self.shipLength, 5))
+        """
+        [player][ship][stat] stats: [0] - number of battles [1] - Dmg [2] - Frags [3] - Wins [4] - PR [post calc] 
+        """
+
         self.nf = set()
         for pI in range(self.totalPlayers):
             players = self.pID[pI]
@@ -104,6 +102,7 @@ class ClanStats:
                     #print('not found: ' + str(shipid))
                     if shipid not in self.nf:
                         self.nf.add(shipid)
+        print('Completed Player Ship Stats')
     #Calculations
     def calcPR(self, pd, ed, pf, ef, pw, ew):
         return 700*self.s.PRnormDmg(pd, ed) + 300*self.s.PRnormKil(pf, ef) + 150*self.s.PRnormWin(pw, ew)
@@ -148,15 +147,15 @@ class ClanStats:
         self.getAllPlayers()
         self.updateExpected()
         self.extractExpected()
+        self.getTotalPlayerStats()
 
     def calcCurrent(self):
         print('Starting Player Ship Stats')
-        self.playerShipStats = np.zeros((self.totalPlayers, self.shipLength, 5))
-        self.playerExpected = np.zeros((self.totalPlayers, 3))
+        #self.playerShipStats = np.zeros((self.totalPlayers, self.shipLength, 5))
         """
         [player][ship][stat] stats: [0] - number of battles [1] - Dmg [2] - Frags [3] - Wins [4] - PR [post calc] 
         """
-        self.getTotalPlayerStats()
+        #self.getTotalPlayerStats()
         print('Completed Player Ship Stats\nStarting Ship PR Calculation')
         self.playerShipStats[:,:,4] = self.calcShipPR(self.playerShipStats, self.expectedStats)
         print('Completed Ship PR Calculation\nStarting Player PR Calculation')
@@ -166,13 +165,14 @@ class ClanStats:
         print('Completed Player PR Calculation')
     
     def saveStats(self):
-        with open('Stats_' + str(self.time) + '.pkl', 'wb') as f:
-            pickle.dump([self.playerShipStats, self.playerOverall, self.shipArrPos, self.expected, self.pID, self.clanList, self.time], f)
+        with open('Stats_' + str(self.sTime) + '.pkl', 'wb') as f:
+            pickle.dump([self.playerShipStats, self.playerOverall, self.shipArrPos, self.expected, self.pID, self.clanList, self.eTime, self.sTime], f)
     
     def getStats(self, name):
         with open(name, 'rb') as f:
-            self.pPlayerShipStats, self.pPlayerOverall, self.pShipArrPos, self.pExpected, self.pPID, self.pClanList, self.ptime = pickle.load(f)
-    
+            self.pPlayerShipStats, self.pPlayerOverall, self.pShipArrPos, self.pExpected, self.pPID, self.pClanList, self.pEtime, self.pSTime  = pickle.load(f)
+        #print(type(self.pShipArrPos))
+
     def usePreLoaded(self, name):
         #binds previous stats to used global variables
         self.playerShipStats = self.pPlayerShipStats
@@ -181,6 +181,7 @@ class ClanStats:
         self.pExpected = self.pExpected
         self.pID = self.pPID
         self.clanList = self.pClanList
+        self.sTime = self.pSTime
     
     def alignPastArray(self, prevData, cPlayers, pPlayers, cShipArr, pShipArr):
         aligned = np.full((len(cPlayers), len(cShipArr), 5), -1) # -1 means no value
@@ -191,35 +192,42 @@ class ClanStats:
 
         shipadj = np.full((len(pPlayers), len(cShipArr), 5), -1) #temporary array: place to dump the ship corrected prevData array
         #Aligns Ships
-        for ship, pos in pShipArr:
+        for ship, pos in pShipArr.items():
             if ship in cShipArr:
-                shipadj[:,cPlayers[ship],:] = prevData[:,pos,:]
+                shipadj[:,cShipArr[ship],:] = prevData[:,pos,:]
         #Aligns Players
         for w in range(len(pPlayers)):
-            if pPlayers[w] in cPlayers:
-                aligned[cPlayers[pPlayers[w]],:,:] = shipadj[w,:,:]
+            if pPlayers[w] in cPD:
+                aligned[cPD[pPlayers[w]],:,:] = shipadj[w,:,:]
         return aligned
     
     def calcRecent(self, name):
-        self.updatePlayerExpected()
         self.getStats(name)
+        #self.updatePlayerExpected()
+        #self.getStats(name)
 
-        self.time_difference = self.time - self.ptime
-
-        aligned = self.alignPastArray(self.pPlayerShipStats, self.PID, self.pPID, self.shipArrPos, self.pShipArr)
+        self.time_difference = self.sTime - self.pSTime
+        
+        aligned = self.alignPastArray(self.pPlayerShipStats, self.pID, self.pPID, self.shipArrPos, self.pShipArrPos)
         self.recent = np.where(aligned != -1, self.playerShipStats - aligned, 0)
         self.recent[:,:,4] = self.calcShipPR(aligned, self.expectedStats)
-        self.recentOverall = self.calcPlayerOverall(difference, self.expectedStats)
+        self.recentOverall = self.calcPlayerOverall(self.recent, self.expectedStats)
     
     #Printing
-    def getStats(self, stats, SN, minbattles):
+    def compileStatsToList(self, stats, SN, minbattles):
         outlist = []
         pr = np.zeros((self.totalPlayers, 5))
         shape = stats.shape
         error = False
         if SN == None:
             if shape == (self.totalPlayers, 5):
-                pr = np.where(self.playerOverall[:,0] >= minbattles,self.playerOverall[:,4],-1)
+                pr = np.transpose(np.where(stats[:,0] >= minbattles,np.transpose(stats),-1))
+                #print(pr)
+                #print(np.transpose( np.transpose(pr[:,1:4]) / pr[:,0] ).shape)
+                pr[:,1:4] = np.transpose( np.transpose(pr[:,1:4]) / pr[:,0] )
+                #print(pr)
+                #print(pr.shape)
+                #exit()
                 outlist = self.produceList(pr)
             else:
                 print('Incorrect Input array')
@@ -230,7 +238,9 @@ class ClanStats:
                     #print(SID)
                     if SID in self.shipArrPos:
                         shippos = self.shipArrPos[SID]
-                        pr = np.where(self.playerShipStats[:,shippos,0] >= minbattles,self.playerShipStats[:,shippos,4],-1)
+                        pr = np.transpose(np.where(stats[:,shippos,0] >= minbattles,np.transpose(stats[:,shippos,:]),-1))
+                        #pr = np.where(stats[:,shippos,0] >= minbattles,stats[:,shippos,:],-1)
+                        pr[:,1:4] = np.transpose( np.transpose(pr[:,1:4]) / pr[:,0] )
                         outlist = self.produceList(pr)
                     else:
                         print('SID Not Found')
@@ -238,14 +248,16 @@ class ClanStats:
                     print('Ship Not Entried')
             else:
                 print('Incorrect Input array')
+        
         return outlist
 
     def produceList(self, pr):
         outlist = []
         for w in range(self.totalPlayers):
-            if pr[w] != -1:
+            if pr[w][0] != -1:
                 pName, ci = self.pName[self.pID[w]]
-                outlist.append([ci,pName,int(np.round(pr[w]))])
+                outlist.append((ci,pName, int(np.round(pr[w][0])),int(np.round(pr[w][1])) , int(np.round(pr[w][2])) , int(np.round(pr[w][3])), int(np.round(pr[w][4]))))
+        #print(outlist)
         return outlist
 
     def printStats(self, stats, SN=None, minbattles=-1):
@@ -253,7 +265,7 @@ class ClanStats:
         SN - Ship Name
         Minbattles - Minimum Required Battles
         """
-        outlist = self.getStats(stats, SN=SN,minbattles=minbattles) 
+        outlist = self.compileStatsToList(stats, SN=SN,minbattles=minbattles) 
         if len(outlist) != 0:
             PRstrTag = 'PR'
             if SN != None:
@@ -263,11 +275,11 @@ class ClanStats:
                 PRstrTag = PRstrTag + ': '
             else:
                 PRstrTag = PRstrTag + ' (Minimum Battles: ' + str(minbattles) + '): '
-            sortedlist = sorted(outlist, key=operator.itemgetter(2), reverse=True) #Sort by pr, clan, name
+            sortedlist = sorted(outlist, key=operator.itemgetter(6), reverse=True) #Sort by pr, clan, name
             #sortedlist = sorted(outlist, key=operator.itemgetter(0, 1)) #Sort by clan, name
             print(PRstrTag)
             for items in sortedlist:
-                print('Clan: [' + self.clanList[items[0]] + '] Player Name: ' + items[1] + ' PR: ' + str(items[2]))
+                print('Clan: ['+self.clanList[items[0]]+'] Player Name: '+str(items[1])+' Battles: '+str(items[2])+' Average Damage: '+str(items[3])+' Average Frags: '+str(items[4])+' Win: '+str(items[5])+' PR: ' + str(items[6]))
     
     def printStatsFromGlobal(self, SN=None, minbattles=-1):
         if SN==None:
@@ -276,6 +288,8 @@ class ClanStats:
             self.printStats(self.playerShipStats, SN=SN, minbattles=minbattles)
     
     def printStatsFromRecent(self, SN=None, minbattles=-1):
+        print('Time of Snapshot: ' + str(time.strftime("%a, %d %b %Y %H:%M:%S UTC", time.gmtime())))
+        print('Time since previous snapshot: ' + str(self.time_difference) + 's')
         if SN==None:
             self.printStats(self.recentOverall, SN=SN, minbattles=minbattles)
         else:
@@ -290,12 +304,16 @@ class ClanStats:
             print(self.a.getShipName(shipid))
 
 if (__name__ == '__main__'):
-    clanstats = ClanStats(['MIA', 'MIA-P', 'MIA-E', 'MIA-I'])
+    clanstats = ClanStats(['MIA', 'MIA-P', 'MIA-E', 'MIA-I', 'MIA-C'])
     clanstats.updatePlayerExpected()
-    clanstats.calcCurrent()
+    #clanstats.calcCurrent()
     #clanstats.printStats(minbattles=6000)
-    clanstats.printStatsFromGlobal(minbattles=20)
+    #clanstats.printStatsFromGlobal(minbattles=20)
     #clanstats.findMissingShips()
+    clanstats.calcRecent('Stats_1554313411.pkl')
+    clanstats.printStatsFromRecent(minbattles=1)
+
+    #clanstats.saveStats()
 
 
 
