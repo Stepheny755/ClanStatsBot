00c796aa80e6aee02f58698de00465ec8e1e36ec
Stephen Yang <stepheny755@gmail.com>
2019-04-11 14:20:50 -0700

    updated front.py frontend

--- front.py
+++ front.py
@@ -7,22 +7,27 @@ import string
 from random import randint
 from util import Util
 
-TEXT_HEIGHT = 60
-TEXT_WIDTH = 300
+CHAR_HEIGHT = 38
+CHAR_WIDTH  = 16
+
+TEXT_HEIGHT = 10
+TEXT_WIDTH = 100
 TEXT_SCALE = 4
-TEXT_FONTSIZE = 60
+
 TEXT_FONTFACE = "fonts/Roboto-Regular.ttf"
 TEXT_FONTFACE_HEADDING = "fonts/Roboto-Regular.ttf"
-# TEXT_COLOUR = (32, 102, 148)
-TEXT_COLOUR_HEADDING = (240, 80, 0)
+TEXT_FONTSIZE = 30
 TEXT_BACKGROUND = (0, 0, 0, 0)
 LINE_SPACING = 5
 
-WHT = (255,255,255)
-RED = (255,0,0)
-GRN = (0,255,0)
+WHT = (255,255,255,255)
+RED = (255,0,0,255)
+GRN = (0,255,0,255)
+YLW = (245,230,83,255)
 
-gencolor = lambda : (randint(50, 255), randint(50, 255), randint(50,255))
+POS_CHAR = '.'
+NEG_CHAR = ','
+NEU_CHAR = '/'
 
 class Front():
 
@@ -30,68 +35,88 @@ class Front():
     gcl = None
     wcl = None
     rcl = None
+    ycl = None
 
-
-    def __init__(self):
+    def __init__(self,format='RGBA'):
         A = self.printable
-
-        image = PIL.Image.new("RGB", (1200,20), (0,0,0))
-        draw = PIL.ImageDraw.Draw(image)
-
-        # make a dictionary of character images
-        xcuts = [draw.textsize(A[:i+1])[0] for i in range(len(A))]
-        xcuts = [0]+xcuts
-        ycut = draw.textsize(A)[1]
-        draw.text((0,0), A, WHT)
-        self.wcl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
-        draw.text((0,0), A, GRN)
-        self.gcl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
-        draw.text((0,0), A, RED)
-        self.rcl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
-
-    # UNUSED FUNCTION, TO BE DELETED
-    def textimage(self,txt,format='RGBA'):
-        print(len(txt))
-        self.txtpos = 0
-
-        image = PIL.Image.new(format, (TEXT_SCALE * TEXT_WIDTH, TEXT_HEIGHT * TEXT_SCALE), TEXT_BACKGROUND)
+        print(string.printable)
+        image = PIL.Image.new(format, (len(A)*CHAR_WIDTH,CHAR_HEIGHT),TEXT_BACKGROUND)
         draw = PIL.ImageDraw.Draw(image)
         font = PIL.ImageFont.truetype(TEXT_FONTFACE, TEXT_FONTSIZE)
-        draw = draw.multiline_text((0,0),txt,gencolor(),font,LINE_SPACING,align="left")
-    	#draw.text((2, 0), txt, TEXT_COLOUR, font = font)
-        return image.resize((TEXT_WIDTH, TEXT_HEIGHT), PIL.Image.ANTIALIAS)
 
-    def make_color(self,text):
-        print("yeet")
-        u = Util()
-        lines = u.countNL(txt)
-        length = u.countLen(txt)
-        return GRN
+        xcuts = [draw.textsize(A[:i+1],font)[0] for i in range(len(A))]
+        xcuts = [0]+xcuts
+        ycut = draw.textsize(A,font)[1]
+        draw.text((0,0), A, WHT, font)
+        self.wcl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
+        draw.text((0,0), A, GRN, font)
+        self.gcl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
+        draw.text((0,0), A, RED, font)
+        self.rcl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
+        draw.text((0,0), A, YLW, font)
+        self.ycl = dict([(A[i], (xcuts[i+1]-xcuts[i]+1, image.crop((xcuts[i]-1, 0, xcuts[i+1], ycut)))) for i in range(len(xcuts)-1)])
 
-    def testimage(self,format = 'RGB'):
-        # Test it...
-        image = PIL.Image.new(format, (TEXT_SCALE * TEXT_WIDTH, TEXT_HEIGHT * TEXT_SCALE), TEXT_BACKGROUND)
 
-        x = 0
+    def testimage(self,txt,format = 'RGBA'):
+
+        txt,width,height = self.formatString(txt)
+
+        image = PIL.Image.new(format, (width,height), TEXT_BACKGROUND)
+
+        x = 5
         y = 0
 
         temp = self.wcl
-        for c in "This is just a nifty text string+22 -341 d\n asdasdasdadsasdasd":
-            if(c == '+'):
+
+        for c in txt:
+            if(c == ' '):
+                x = x + CHAR_WIDTH
+            elif(c == POS_CHAR):
                 temp = self.gcl
-            elif(c == '-'):
+            elif(c == NEG_CHAR):
                 temp = self.rcl
+            elif(c == NEU_CHAR):
+                temp = self.ycl
             elif(c == ' '):
                 temp = self.wcl
+            if(c == '\\'):
+                y = y + CHAR_HEIGHT
+                x = 5
+                continue
 
             w, char_image = temp[c]
-            image.paste(char_image, xy)
+            image.paste(char_image,(x,y))
             x += w
 
-        #image2.show()
-        return image
+        image.show()
+        return image.resize((width, height), PIL.Image.ANTIALIAS)
+
+    def formatString(self,txt):
+        #Format:
+        #Overall | PR: xxxx.xx Battles: xxxx DMG: xxxxx.xx WR: xx.xx% Kills: x.xx
+        #Weekly  | PR: +xxxx.x Battles: +xxx DMG: +xxxxx.x WR: +xx.x% Kills: +x.x
+        #Monthly | PR: +xxxx.x Battles: +xxx DMG: +xxxxx.x WR: +xx.x% Kills: +x.x
+
+        u = Util()
+
+        ostats = "Overall | PR: xxxx.xx Battles: xxxx DMG: xxxxx.xx WR: xx.xx% Kills: x.xx"
+        wstats = "Weekly | PR: +xxxx.x Battles: +xxx DMG: +xxxxx.x WR: +xx.x% Kills: +x.x"
+        mstats = "Monthly | PR: +xxxx.x Battles: +xxx DMG: +xxxxx.x WR: +xx.x% Kills: +x.x"
+        
+        a = str.splitlines(txt)
+        print(a)
+
+        #max(lambda x : x = len())
+
+        formatted = "\\".join(a)
+        width = (CHAR_WIDTH * u.countLen(a[0]))
+        print(width)
+        height = (CHAR_HEIGHT * (u.countNL(formatted)+1))
+
+        return (formatted,width,height)
+
 
 if(__name__=="__main__"):
     f = Front()
-    #f.textimage("what is gonig on\n+123123 asd\nyeet -12344\nyike").save('texttest.png')
-    f.testimage().save('yeet.png')
+    f.testimage("Overall | PR: xxxx.xx Battles: xxxx DMG: xxxxx.xx WR: xx.xx% Kills: x.xx\nWeekly | PR: +xxxx.x Battles: +xxx DMG: +xxxxx.x WR: +xx.x% Kills: +x.x\nMonthly| PR: +xxxx.x Battles: +xxx DMG: +xxxxx.x WR: +xx.x% Kills: +x.x").save('yeet.png')
+    #f.testimage("what is gonig on\\+123123 asd\\yeet -12344\\yike").save('yeet.png')
